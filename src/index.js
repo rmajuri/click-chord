ed45889999e4807ed877f8a10f3b6c14c4188c9c
Rob Majuri <robmajuri@gmail.com>
2019-10-23 20:19:55 -0500

    Fix root config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './global-style.css'
-import App from '../app/main'
+import App from './app/main'
 
 ReactDOM.render(<App />, document.getElementById('root'))
